6fd1485d0a59f21c0422fe5f489cdc47eed8adf4
Joshua Rwanda <joshuarwanda@gmail.com>
2024-06-13 19:15:38 +0300

    working on problem counting max num of ones in an array

--- DSA/maxOnesCounter.py
+++ DSA/maxOnesCounter.py
@@ -1,1 +1,16 @@
+# Solve error :Name_error: List is not defined
+class Solution:
+    def findMaxConsOnes(self, nums: list[int]) -> int:
+        max_count = 0
+        current_count = 0
 
+        for num in nums:
+            if num == 1:
+                current_count += 1
+            else if num == 0:
+                # detect when the current counting window has closed
+                print('Current window closed')
+                max_count = max(current_count, max_count)
+                continue
+        max_count = max(current_count, max_count)
+        return max_count
